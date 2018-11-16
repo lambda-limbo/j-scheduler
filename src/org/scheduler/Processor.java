7f90d023e79ac367641f3b8a79bfb079e8f54150
Rafael Campos Nunes <rafaelnunes@engineer.com>
2018-11-16 08:46:11 -0200

    Renames the class
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

---
+++ src/org/scheduler/Processor.java
@@ -0,0 +1,14 @@
+package org.scheduler;
+
+import org.scheduler.Process;
+
+public class Processor {
+
+    public Processor() {
+        
+    }
+
+    public void feed(Process p) {
+        
+    }
+}
