e0360c00b3b9f9c62fe66f713e0dfbcbdf09d35f
Rafael Campos Nunes <rafaelnunes@engineer.com>
2018-11-16 08:43:01 -0200

    Make the application use the native look and feel
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- src/org/Main.java
+++ src/org/Main.java
@@ -1,8 +1,18 @@
 package org;
 
+import javax.swing.*;
+
+import org.gui.Greetings;
+
 public class Main {
 
     public static void main(String args[]) {
-        System.out.printf("Hello scheduler");
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch(Exception ex) {
+            ex.printStackTrace();
+        }
+
+        SwingUtilities.invokeLater(new Greetings());
     }
 }
