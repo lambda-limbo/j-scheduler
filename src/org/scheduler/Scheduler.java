6be2820157293a1f5243420e893d9a3d0def800a
Rafael Campos Nunes <rafaelnunes@engineer.com>
2018-11-19 15:17:58 -0200

    Fix the declaration of the process table
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- src/org/scheduler/Scheduler.java
+++ src/org/scheduler/Scheduler.java
@@ -1,8 +1,11 @@
 package org.scheduler;
 
+import java.util.Queue;
+import java.util.PriorityQueue;
+
 public class Scheduler {
 
-    Queue<Process> processQueue = new PriorityQueue();
+    Queue<Process> processQueue = new PriorityQueue<>();
 
     public Scheduler() {
         
